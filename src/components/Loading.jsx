07291e10bb2ee6930506f9559d30f7b9ba0a62fa
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-09-15 00:37:55 -0300

    Estilizaçao do component Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 class Loading extends Component {
   render() {
     return (
-      <p>Carregando...</p>
+      <div className="d-flex align-items-center text-success">
+        <strong>Carregando...</strong>
+        <div className="spinner-border ms-auto m-2" role="status" aria-hidden="true" />
+      </div>
     );
   }
 }
