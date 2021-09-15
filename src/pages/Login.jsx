0836b4fc8d44be2b969ff0c689bf85e2685adbb6
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-09-14 23:00:00 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,74 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {
+        name: '',
+      },
+      loading: false,
+      redirect: false,
+    };
+  }
+
+  handleChangeInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      user: {
+        [name]: value,
+      },
+    });
+  }
+
+  logar = async () => {
+    const { user } = this.state;
+    this.setState({ loading: true });
+    const setUser = await createUser(user);
+    if (setUser === 'OK') {
+      this.setState({ redirect: true });
+    }
+  };
+
+  renderLogin = () => {
+    const { user: { name } } = this.state;
+    const MIN_LENGTH = 3;
+    return (
+      <>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="input-name">
+            <input
+              id="input-name"
+              name="name"
+              value={ name }
+              onChange={ this.handleChangeInput }
+              data-testid="login-name-input"
+              type="text"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            onClick={ this.logar }
+            disabled={ name.length < MIN_LENGTH }
+          >
+            Entrar
+          </button>
+        </form>
+      </>
+    );
+  }
+
   render() {
+    const { loading, redirect } = this.state;
     return (
       <div data-testid="page-login">
-        <h1>Login</h1>
+        { loading ? <Loading /> : this.renderLogin() }
+        { redirect && <Redirect to="/search" /> }
       </div>
     );
   }
